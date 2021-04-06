06d5ec4193fac6f09d082448ffffe8a925daeb60
caolei <caolei.fnst@cn.fujitsu.com>
2021-04-06 09:57:03 +0800

    2021/4/6 [init] project push script...

--- delay.sh
+++ delay.sh
@@ -4,7 +4,8 @@
 set -e
 
 # 生成静态文件
-npm run build:md
+#npm run build:md
+npm run docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -24,6 +25,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/springxing/xyc.github.io.git master
+git push -f https://github.com/springxing/xyc.github.io.git master:github-pages
 
 cd -
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: '小洋葱',
   description: '描述',
-  base: '/xyc/', // 这是部署到github相关的配置 下面会讲
+  base: '/', // 这是部署到github相关的配置 下面会讲
   // 注入到当前页面的 HTML <head> 中的标签
   head: [
     ['link', { rel: 'icon', href: '/assets/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
